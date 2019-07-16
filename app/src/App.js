40911ff1cc7b532aace0a78e39ad199fdf341615
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-15 20:14:12 -0400

    Added function for generating blocks of grid.

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  state = {
+    blocks: []
+  };
+  componentDidMount() {
+    const blocks = generateBlocks();
+    this.setState({
+      blocks: blocks
+    });
+  }
+
+  render() {
+    return <div className="App" />;
+  }
+}
+
+function generateBlocks() {
+  const array = new Array(400);
+  for (let i = 0; i < 400; i++) {
+    let object = {
+      id: i,
+      alive: false
+    };
+    array[i] = object;
+  }
+  return array;
 }
 
 export default App;
