298d6db16c2edc58d73aeb43084ba0649807dd8c
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-17 22:08:24 -0400

    Pause functionality working. NextGrid could not be its own function if it needs access to setInterval.

--- app/src/App.js
+++ app/src/App.js
@@ -17,18 +17,20 @@ class App extends Component {
   }
 
   toggleBlock = id => {
-    this.setState({
-      ...this.state,
-      blocks: this.state.blocks.map(block => {
-        if (block.id === id) {
-          return {
-            ...block,
-            alive: !block.alive
-          };
-        }
-        return block;
-      })
-    });
+    if (this.state.generation === 0) {
+      this.setState({
+        ...this.state,
+        blocks: this.state.blocks.map(block => {
+          if (block.id === id) {
+            return {
+              ...block,
+              alive: !block.alive
+            };
+          }
+          return block;
+        })
+      });
+    }
   };
 
   initializeGrid() {
@@ -44,40 +46,40 @@ class App extends Component {
 
   beginGame = () => {
     if (this.state.generation === 0) {
-      const timer = setInterval(this.nextGrid, 200);
+      const timer = setInterval(() => {
+        if (this.state.paused) {
+          clearInterval(timer);
+        }
+        const { blocks, neighbors } = this.state;
+        const newBlocks = blocks.map(block => {
+          let aliveNeighbors = 0;
+          let neighborIndices = neighbors[block.id];
+          neighborIndices.forEach(index => {
+            if (blocks[index].alive) {
+              aliveNeighbors += 1;
+            }
+          });
+          if (block.alive) {
+            if (aliveNeighbors === 2 || aliveNeighbors === 3) {
+              return block;
+            }
+            block = { ...block, alive: false };
+            return block;
+          } else {
+            if (aliveNeighbors === 3) {
+              block = { ...block, alive: true };
+              return block;
+            }
+            return block;
+          }
+        });
+        this.setState({
+          ...this.state,
+          generation: this.state.generation + 1,
+          blocks: newBlocks
+        });
+      }, 50);
     }
-  };
-
-  nextGrid = () => {
-    console.log(this.state.generation);
-    const { blocks, neighbors } = this.state;
-    const newBlocks = blocks.map(block => {
-      let aliveNeighbors = 0;
-      let neighborIndices = neighbors[block.id];
-      neighborIndices.forEach(index => {
-        if (blocks[index].alive) {
-          aliveNeighbors += 1;
-        }
-      });
-      if (block.alive) {
-        if (aliveNeighbors === 2 || aliveNeighbors === 3) {
-          return block;
-        }
-        block = { ...block, alive: false };
-        return block;
-      } else {
-        if (aliveNeighbors === 3) {
-          block = { ...block, alive: true };
-          return block;
-        }
-        return block;
-      }
-    });
-    this.setState({
-      ...this.state,
-      generation: this.state.generation + 1,
-      blocks: newBlocks
-    });
   };
 
   switchBuffers = () => {
@@ -89,12 +91,13 @@ class App extends Component {
   };
 
   restartGame = () => {
-    const blocks = generateBlocks();
+    const blocks = generateBlocks(20, 20);
     const flattened = blocks.reduce((acc, val) => acc.concat(val));
     this.setState({
       ...this.state,
       blocks: flattened,
-      generation: 0
+      generation: 0,
+      paused: false
     });
   };
 
@@ -105,24 +108,25 @@ class App extends Component {
   };
 
   populateGrid = () => {
-    const { blocks } = this.state;
-    const randomNumbers = randomNumber(Date.now(), blocks.length);
-    console.log(randomNumbers);
-    const newBlocks = blocks.concat();
-    const length = randomNumbers.length;
-    for (let i = 0; i < length; i++) {
-      const current = randomNumbers[i];
-      if (current in newBlocks) {
-        newBlocks[current] = {
-          ...newBlocks[current],
-          alive: true
-        };
+    const { blocks, generation } = this.state;
+    if (generation === 0) {
+      const randomNumbers = randomNumber(Date.now(), blocks.length);
+      const newBlocks = blocks.concat();
+      const length = randomNumbers.length;
+      for (let i = 0; i < length; i++) {
+        const current = randomNumbers[i];
+        if (current in newBlocks) {
+          newBlocks[current] = {
+            ...newBlocks[current],
+            alive: true
+          };
+        }
       }
+      this.setState(prevState => ({
+        ...prevState,
+        blocks: newBlocks
+      }));
     }
-    this.setState(prevState => ({
-      ...prevState,
-      blocks: newBlocks
-    }));
   };
 
   render() {
